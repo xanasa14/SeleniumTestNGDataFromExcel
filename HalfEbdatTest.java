3d8b4267258a9f459899610aff694657a4873da0
Xavier Navarro <xanasa14@hotmail.com>
2019-04-15 00:56:33 -0400

    Update HalfEbdatTest.java

--- HalfEbdatTest.java
+++ HalfEbdatTest.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
@@ -5,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -16,6 +18,7 @@ public class HalfEbdatTest {
 	@BeforeMethod
 	public void setU() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\chromeDriver\\chromedriver.exe");
+	
 		driver = new ChromeDriver(); 
 		driver.manage().window().maximize(); 
 		driver.manage().deleteAllCookies();
@@ -28,10 +31,12 @@ public class HalfEbdatTest {
 	public Iterator<Object[]> getTestData(){
 		ArrayList<Object[]> testData = TestUtil.getDatafromExcel();
 		return testData.iterator();
+		
+ 
 	}
 	
 	@Test(dataProvider ="getTestData")
-	public void halfEbayRegPageTest(String firstName, String lastName, String email, String password) {	
+	public void halfEbayRegPageTest(String firstName, String lastName, String e, String password) {	
 	
 		driver.findElement(By.xpath("//*[@id=\"firstname\"]")).clear();
 		driver.findElement(By.xpath("//*[@id=\"firstname\"]")).sendKeys(firstName);
@@ -40,10 +45,15 @@ public class HalfEbdatTest {
 		driver.findElement(By.xpath("//*[@id=\"lastname\"]")).sendKeys(lastName);
 		
 		driver.findElement(By.xpath("//*[@id=\"email\"]")).clear();
-		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys(email);
+		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys(e);
 		
 		driver.findElement(By.xpath("//*[@id=\"PASSWORD\"]")).clear();
 		driver.findElement(By.xpath("//*[@id=\"PASSWORD\"]")).sendKeys(password);
 	
 }
+	@AfterMethod
+	public void TeadDown() {
+		driver.quit();
+	}
+	
 }
